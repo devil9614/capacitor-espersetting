239a595b53ec2baed1b039e584069f0e28f871bf
devil9614 <sujanmukherjee9614@gmail.com>
2024-01-09 13:32:49 +0530

    update

--- android/src/main/java/com/nowpurchase/plugins/example/CapacitorEsperSetting.java
+++ android/src/main/java/com/nowpurchase/plugins/example/CapacitorEsperSetting.java
@@ -1,8 +1,13 @@
-package com.nowpurchase.plugins.example;
+package com.nowpurchase.CapacitorEsperSetting;
 
 import android.util.Log;
+import android.util.Log;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 
-public class Example {
+public class CapacitorEsperSetting {
 
     public String echo(String value) {
         Log.i("Echo", value);
